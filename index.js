ca41976adefccbb23f59dd15b8765f774392c69e
Brian Arango <brianvictus@gmail.com>
2019-07-25 18:45:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ function produceDrivingRange( blockRange ) {
     let difference = blockRange - distanceToTravel;
 
     if ( difference > 0 ) {
-      return `within range by ${difference}`
+      return `within range by ${difference}`;
     } else {
-      return `${Math.abs(difference)} blocks out of range`
+      return `${Math.abs(difference)} blocks out of range`;
     }
-  }
+  };
 }
 
 function produceTipCalculator( percentage ) {
   return function ( rideFare ) {
     return rideFare * percentage;
-  }
+  };
 }
 
 function createDriver() {
-  let driverId = 0
+  let driverId = 0;
   return class {
     constructor( name ) {
-      this.id = ++driverId
-      this.name = name
+      this.id = ++driverId;
+      this.name = name;
     }
-  }
+  };
 }
